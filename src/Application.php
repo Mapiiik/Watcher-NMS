62dddfaf7c71fa3783fed1fa4060e9fe92d027d6
mapik <mapik@Mapik-Silencio>
2020-02-15 13:45:31 +0100

    Basic configuration

--- src/Application.php
+++ src/Application.php
@@ -55,6 +55,10 @@ class Application extends BaseApplication
         }
 
         // Load more plugins here
+
+        // CakeDC/users
+        $this->addPlugin(\CakeDC\Users\Plugin::class);
+        Configure::write('Users.config', ['users']);       
     }
 
     /**
