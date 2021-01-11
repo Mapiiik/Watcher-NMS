dd0625cbb0b640f5908df5d5f86983b5dc8719f0
mapik <mapik@Mapik-Silencio>
2021-01-11 16:05:11 +0100

    added Google OAuth authentication support

--- config/users.php
+++ config/users.php
@@ -2,4 +2,8 @@
 return [
     'Users.Registration.active' => false,
     'Users.Superuser.allowedToChangePasswords' => true,
+    'Users.Social.login' => true,
+    'Auth.AuthenticationComponent.loginRedirect' => '/users/profile',
+    'OAuth.providers.google.options.clientId' => env('GOOLE_OAUTH_CLIENT_ID', null),
+    'OAuth.providers.google.options.clientSecret' => env('GOOLE_OAUTH_CLIENT_SECRET', null),
 ];
