c1632d020ba6ddcedb78372e50364893ed22dd37
Martin Patocka <mapik@mapik.net>
2023-06-14 13:54:42 +0200

    Permissions - code cleanup

--- config/permissions.php
+++ config/permissions.php
@@ -9,6 +9,9 @@
  * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
+use Cake\Http\ServerRequest;
+use Cake\Utility\Hash;
+
 /*
  * This is a quick roles-permissions implementation
  * Rules are evaluated top-down, first matching rule will apply
@@ -127,8 +130,8 @@ $permissions = [
             'plugin' => null,
             'controller' => 'AppUsers',
             'action' => 'resetOneTimePasswordAuthenticator',
-            'allowed' => function (array $user, $role, \Cake\Http\ServerRequest $request) {
-                $userId = \Cake\Utility\Hash::get($request->getAttribute('params'), 'pass.0');
+            'allowed' => function (array $user, $role, ServerRequest $request) {
+                $userId = Hash::get($request->getAttribute('params'), 'pass.0');
                 if (!empty($userId) && !empty($user)) {
                     return $userId === $user['id'];
                 }
