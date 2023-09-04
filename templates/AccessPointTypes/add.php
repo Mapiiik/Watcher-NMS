3258d53540de720491f4532f891c73be71b65d2e
Martin Patocka <mapik@mapik.net>
2023-09-04 19:32:26 +0200

    Remove unused creators/modifiers variables

--- templates/AccessPointTypes/add.php
+++ templates/AccessPointTypes/add.php
@@ -2,8 +2,6 @@
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\AccessPointType $accessPointType
- * @var \Cake\Collection\CollectionInterface|array<string> $creators
- * @var \Cake\Collection\CollectionInterface|array<string> $modifiers
  */
 ?>
 <div class="row">
