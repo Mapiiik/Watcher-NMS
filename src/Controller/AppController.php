addab07b6ee28edc3898356cfe170fc400e4afeb
Martin Patocka <mapik@mapik.net>
2023-10-09 08:43:36 +0000

    Code cleanup - moving custom functions from AppController

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -17,7 +17,6 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use AuditLog\Meta\RequestMetadata;
-use Cake\Cache\Cache;
 use Cake\Controller\Controller;
 use Cake\Core\Configure;
 use Cake\Datasource\Paging\PaginatedInterface;
@@ -195,72 +194,6 @@ class AppController extends Controller
 
         return Router::url(
             ['?' => $query + $request->getQueryParams()] + $request->getParam('pass')
-        );
-    }
-
-    /**
-     * Check if Git binary is callable.
-     *
-     * @return bool
-     */
-    public static function gitBinaryCallable(): bool
-    {
-        exec('git > /dev/null 2>&1', $response, $exit_code);
-
-        return $exit_code === 1;
-    }
-
-    /**
-     * Check if Git repository is present.
-     *
-     * @return bool
-     */
-    public static function gitRepositoryPresent(): bool
-    {
-        return file_exists(dirname(__DIR__, 2) . '/.git');
-    }
-
-    /**
-     * Get application version if installed from Git.
-     *
-     * @return string
-     */
-    public static function getVersion(): string
-    {
-        return Cache::remember(
-            'app_version',
-            function () {
-                if (AppController::gitRepositoryPresent() && AppController::gitBinaryCallable()) {
-                    $version = shell_exec('git describe --tags 2>/dev/null');
-                    if (is_string($version)) {
-                        return rtrim($version);
-                    }
-                }
-
-                return __('Not installed via Git');
-            }
-        );
-    }
-
-    /**
-     * Get application changelog if installed from Git.
-     *
-     * @return string
-     */
-    public static function getChangelog(): string
-    {
-        return Cache::remember(
-            'app_changelog',
-            function () {
-                if (AppController::gitRepositoryPresent() && AppController::gitBinaryCallable()) {
-                    $changelog = shell_exec('git log -10 --decorate=short');
-                    if (is_string($changelog)) {
-                            return rtrim($changelog);
-                    }
-                }
-
-                return __('Not installed via Git');
-            }
         );
     }
 }
